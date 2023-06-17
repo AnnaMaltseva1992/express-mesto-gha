16e2adc5a0121390d61e459a2e63f72620f96aa4
Anna Maltseva <mr-sbazin@yandex.ru>
2023-06-17 19:29:03 +0300

    fix: исправлен импорт

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
 const { ERROR_CODE_NOT_FOUND } = require('../errors/errors');
-const { auth } = require('../middlewares/auth');
+const auth = require('../middlewares/auth');
 
 router.use('/users', auth, userRoutes);
 router.use('/cards', auth, cardRoutes);
