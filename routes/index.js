384d9245f7eff1a33169bf0652938de38ccb65be
Anna Maltseva <mr-sbazin@yandex.ru>
2023-06-20 13:42:02 +0300

    fix: исправлены роуты аутентификации и авторизации

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
-const { createUser, login } = require('../controllers/users');
+// const { createUser, login } = require('../controllers/users');
 const auth = require('../middlewares/auth');
-const { validationSignin, validationSignup } = require('../validation/validation');
+// const { validationSignin, validationSignup } = require('../validation/validation');
 const NotFoundError = require('../errors/notFoundError');
 
 router.use('/users', auth, userRoutes);
@@ -11,8 +11,8 @@ router.use('/cards', auth, cardRoutes);
 
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);
-router.post('/signin', validationSignin, login);
-router.post('/signup', validationSignup, createUser);
+// router.post('/signin', validationSignin, login);
+// router.post('/signup', validationSignup, createUser);
 
 router.use('/*', auth, (req, res, next) => {
   next(new NotFoundError('Профиль не найден'));
